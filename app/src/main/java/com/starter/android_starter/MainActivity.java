71cbf9f611f5c6611a67af5cfbd900e175aec464
varsha2409 <varsha.ezihsz@gmail.com>
2016-10-30 22:04:20 +0530

    Update MainActivity.java

--- app/src/main/java/com/starter/android_starter/MainActivity.java
+++ app/src/main/java/com/starter/android_starter/MainActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+private static final String TWITTER_KEY = "your twitter key";
+    private static final String TWITTER_SECRET = "your twitter secret";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+          TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this,Login.class);
         startActivity(intent);
